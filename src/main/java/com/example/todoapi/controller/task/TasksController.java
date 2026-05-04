65ba05dd5b6581e733ba8146d66e18dcc10d2e03
yamato2323 <dev@gaobusystem.com>
2026-05-04 20:59:34 +0900

    POST されたら 201 を返す

--- src/main/java/com/example/todoapi/controller/task/TasksController.java
+++ src/main/java/com/example/todoapi/controller/task/TasksController.java
@@ -4,6 +4,7 @@ import com.example.todo_api.model.TaskDTO;
 import com.example.todoapi.controller.TasksApi;
 import com.example.todoapi.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TasksController implements TasksApi {
 
     private final TaskService taskService;
+
     @Override
     public ResponseEntity<TaskDTO> showTask(Long taskId){
         var entity = taskService.find(taskId);
@@ -20,4 +22,11 @@ public class TasksController implements TasksApi {
         dto.setTitle(entity.getTitle());
         return ResponseEntity.ok(dto);
     }
+
+    @Override
+    public ResponseEntity<Void> createTask(com.example.todo_api.model.CreateTaskRequest createTaskRequest){
+        // 受け取った createTaskRequest を使ってサービスを呼び出す処理などをここに書く
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 }
