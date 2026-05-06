6f5763f7bb5ab79ef8edfce7882ac909b981c71e
yamato2323 <dev@gaobusystem.com>
2026-05-06 13:18:10 +0900

    400 Bad Request（フィールド欠損）

---
+++ src/main/java/com/example/todoapi/controller/advice/BadReruestErrrorCreatetor.java
@@ -0,0 +1,32 @@
+package com.example.todoapi.controller.advice;
+
+import com.example.todo_api.model.BadRequestError;
+import com.example.todo_api.model.InvalidParam;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BadReruestErrrorCreatetor {
+
+    public static BadRequestError form(MethodArgumentNotValidException ex) {
+        var invalidParamList = createInvalidParamList(ex);
+        var error = new BadRequestError();
+        error.setInvalidParams(invalidParamList);
+        return error;
+    }
+    private static List<InvalidParam> createInvalidParamList(MethodArgumentNotValidException ex){
+        return ex.getFieldErrors()
+                .stream()
+                .map(fieldError -> createInvalidParam(fieldError))
+                .collect(Collectors.toList());
+    }
+
+    private static InvalidParam createInvalidParam(FieldError fieldError){
+        var invalidParam = new InvalidParam();
+        invalidParam.setName(fieldError.getField());
+        invalidParam.setReason(fieldError.getDefaultMessage());
+        return invalidParam;
+    }
+}
--- src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
@@ -1,14 +1,22 @@
 package com.example.todoapi.controller.advice;
 
+import com.example.todo_api.model.BadRequestError;
 import com.example.todo_api.model.ResourceNotFoundError;
 import com.example.todoapi.service.task.TaskEntityNotFoundException;
+import org.hibernate.validator.constraints.Normalized;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(TaskEntityNotFoundException.class)
     public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(TaskEntityNotFoundException e){
@@ -18,4 +26,17 @@ public class CustomExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(error);
     };
+
+    @Nullable
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request)
+    {
+
+        var error =  BadReruestErrrorCreatetor.form(ex);
+        return ResponseEntity.badRequest().body(error);
+    }
 }
