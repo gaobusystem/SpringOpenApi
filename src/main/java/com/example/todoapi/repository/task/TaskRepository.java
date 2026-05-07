1b99fb5c7028b5f5377cfc55e81aa97ff254f48a
yamato2323 <dev@gaobusystem.com>
2026-05-07 11:03:40 +0900

    タスクAPI一覧　   200 OK：Repository

--- src/main/java/com/example/todoapi/repository/task/TaskRepository.java
+++ src/main/java/com/example/todoapi/repository/task/TaskRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -13,7 +14,12 @@ public interface TaskRepository {
     @Select("Select id, title From tasks where id = #{taskId} ")
     Optional<TaskRecord> select(long taskId);
 
+    @Select("select id, title from tasks")
+    List<TaskRecord> selectList();
+
     @Options(useGeneratedKeys = true, keyProperty = "id")
     @Insert("Insert into tasks (title) values (#{title})")
     void insert(TaskRecord record);
+
+
 }
--- src/main/java/com/example/todoapi/service/task/TaskService.java
+++ src/main/java/com/example/todoapi/service/task/TaskService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,9 +28,9 @@ public class TaskService {
     }
 
     public List<TaskEntity> find() {
-        return List.of(
-                new TaskEntity(1L, "title"),
-                new TaskEntity(2, "title")
-        );
+        return taskRepository.selectList()
+                .stream()
+                .map(record -> new TaskEntity(record.getId(), record.getTitle()))
+                .collect(Collectors.toList());
     }
 }
