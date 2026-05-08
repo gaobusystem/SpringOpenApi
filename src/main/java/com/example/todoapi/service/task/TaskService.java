132ceff1768f9cd6f497e42bd8375709b65c9dc9
yamato2323 <dev@gaobusystem.com>
2026-05-08 16:40:15 +0900

    タスク削除API　　 404 Not Found：Service

--- src/main/java/com/example/todoapi/service/task/TaskService.java
+++ src/main/java/com/example/todoapi/service/task/TaskService.java
@@ -43,6 +43,8 @@ public class TaskService {
     }
 
     public void delete(Long taskId) {
+        taskRepository.select(taskId)
+                .orElseThrow(() -> new TaskEntityNotFoundException((taskId)));
         taskRepository.delete(taskId);
     }
 }
